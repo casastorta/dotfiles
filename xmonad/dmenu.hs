492e34104b860c92f15c5834b802f9e3359dea5f
Vedran Krivokuca <vkrivokuca+github@gmail.com>
2012-01-11 09:35:19 +0100

    ...

--- xmonad/dmenu.hs
+++ xmonad/dmenu.hs
@@ -1,1 +1,32 @@
-/home/vedran/Dropbox/.linuxanja/home/.xmonad-sync/dmenu.hs
+------------------------------------------------------------------------------
+-- |
+-- Module      :  DMenu 
+-- Copyright   :  (c) Mads N Noe 2009
+-- Maintainer  :  mntnoe (@) gmail.com
+-- License     :  as-is
+-- 
+-- DMenu helper functions.
+-- 
+------------------------------------------------------------------------------
+
+module DMenu where
+
+-- Haskell modules
+import Data.List (intercalate)
+
+-- XMonad modules
+import XMonad.Prompt
+
+-- | Run command in path.
+dmenuRun xpc = intercalate " " $ "dmenu_run" : dmenuArgs xpc "Run:"
+
+-- | DMenu options based on an XPC.
+dmenuArgs xpc prompt = 
+    [ "-b"
+    , "-fn" , font     xpc
+    , "-nb" , bgColor  xpc
+    , "-nf" , fgColor  xpc
+    , "-sb" , bgHLight xpc
+    , "-sf" , fgHLight xpc
+    , "-p"  , prompt
+    ]
